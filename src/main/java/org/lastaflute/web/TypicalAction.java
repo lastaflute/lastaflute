6f3e25095488128bab5d9ca9867fa2a317ae066d
jflute <dbflute@gmail.com>
2015-06-24 11:45:16 +0900

    prepare LaDocs

--- src/main/java/org/lastaflute/web/TypicalAction.java
+++ src/main/java/org/lastaflute/web/TypicalAction.java
@@ -24,6 +24,7 @@ import javax.annotation.Resource;
 import org.dbflute.jdbc.Classification;
 import org.dbflute.optional.OptionalThing;
 import org.dbflute.util.Srl;
+import org.lastaflute.core.direction.LaDocs;
 import org.lastaflute.core.exception.ExceptionTranslator;
 import org.lastaflute.core.exception.LaApplicationException;
 import org.lastaflute.core.time.TimeManager;
@@ -58,7 +59,7 @@ import org.slf4j.LoggerFactory;
  * And you can add methods for all applications.
  * @author jflute
  */
-public abstract class TypicalAction extends LastaAction implements ActionHook {
+public abstract class TypicalAction extends LastaAction implements ActionHook, LaDocs {
 
     // ===================================================================================
     //                                                                          Definition
@@ -353,86 +354,5 @@ public abstract class TypicalAction extends LastaAction implements ActionHook {
             final String msg = sb.toString();
             throw new ForcedRequest404NotFoundException(msg, e);
         }
-    }
-
-    // ===================================================================================
-    //                                                                            Document
-    //                                                                            ========
-    /**
-     * <pre>
-     * [AtMark]Execute
-     * public HtmlResponse index() {
-     *     ListResultBean&lt;Product&gt; memberList = productBhv.selectList(cb -> {
-     *         cb.query().addOrderBy_RegularPrice_Desc();
-     *         cb.fetchFirst(3);
-     *     });
-     *     List&lt;MypageProductBean&gt; beans = memberList.stream().map(member -> {
-     *         return new MypageProductBean(member);
-     *     }).collect(Collectors.toList());
-     *     return asHtml(path_Mypage_MypageJsp).renderWith(data -> {
-     *         data.register("beans", beans);
-     *     });
-     * }
-     * </pre>
-     */
-    protected void documentOfAll() {
-    }
-
-    /**
-     * <pre>
-     * <span style="font-size: 130%; color: #553000">[Validation]</span>
-     * o validate(form, moreValidationLambda, validationErrorLambda) <span style="color: #3F7E5E">// validation by annotation and program</span>
-     * o validateApi(form, moreValidationLambda) <span style="color: #3F7E5E">// for API</span>
-     * 
-     * <span style="font-size: 130%; color: #553000">[Response]</span>
-     * o asHtml(HTML template) <span style="color: #3F7E5E">// return response as HTML by template e.g. JSP</span>
-     * o asJson(JSON bean) <span style="color: #3F7E5E">// return response as JSON from bean</span>
-     * o asStream(file name for Stream) <span style="color: #3F7E5E">// return response as stream from input stream</span>
-     * o asXml(XML string) <span style="color: #3F7E5E">// return response as stream from input stream</span>
-     * 
-     * <span style="font-size: 130%; color: #553000">[Routing]</span>
-     * o redirect(actionType) <span style="color: #3F7E5E">// redirect to the action, /product/list</span>
-     * o redirectById(actionType, ids) <span style="color: #3F7E5E">// by the ID, /product/list/3</span>
-     * o redirectByParam(actionType, params) <span style="color: #3F7E5E">// by the GET parameter, /product/list?sea=mystic</span>
-     * o redirectWith(actionType, moreUrl_or_params) <span style="color: #3F7E5E">// with various parameters</span>
-     * o movedPermanently(response) <span style="color: #3F7E5E">// 301 redirect</span>
-     * 
-     * o forward(actionType) <span style="color: #3F7E5E">// redirect to the action, /product/list</span>
-     * o forwardById(actionType, ids) <span style="color: #3F7E5E">// by the ID, /product/list/3</span>
-     * o forwardByParam(actionType, params) <span style="color: #3F7E5E">// by the GET parameter, /product/list?sea=mystic</span>
-     * o forwardWith(actionType, moreUrl_or_params) <span style="color: #3F7E5E">// with various parameters</span>
-     * 
-     * <span style="font-size: 130%; color: #553000">[Advance]</span>
-     * o async(noArgLambda) <span style="color: #3F7E5E">// execute asynchronous process</span>
-     * o requiresNew(txLambda) <span style="color: #3F7E5E">// execute new transaction process</span>
-     * 
-     * <span style="font-size: 130%; color: #553000">[User Info]</span>
-     * o getUserBean() <span style="color: #3F7E5E">// get bean of login user</span>
-     * 
-     * <span style="font-size: 130%; color: #553000">[Verify]</span>
-     * o verifyParameterExists()
-     * o verifyParameterTrue()
-     * o verifyTrueOr404NotFound()
-     * o verifyTrueOrIllegalTransition()
-     * 
-     * <span style="font-size: 130%; color: #553000">[Small Helper]</span>
-     * o currentDate() <span style="color: #3F7E5E">// current date as LocalDate</span>
-     * o currentDateTime() <span style="color: #3F7E5E">//current date-time as LocalDateTime</span>
-     * o isEmpty()
-     * o isNotEmpty()
-     * o isCls(cdefType, code) <span style="color: #3F7E5E">// determine the code matches the classification</span>
-     * o toCls(cdefType, code) <span style="color: #3F7E5E">// convert the code to the classification</span>
-     * </pre>
-     */
-    protected void documentOfMethods() {
-    }
-
-    /**
-     * <pre>
-     * o Cls : is Classification (CDef)
-     * o CDef : is auto-generated ENUM as Classification Definition
-     * </pre>
-     */
-    protected void documentOfWordDictionary() {
     }
 }
