0dc596d45e0dc2b671322b4e0bbc324d72fd46db
jflute <dbflute@gmail.com>
2016-08-08 19:51:54 +0900

    ClassificationProvider for appcls

--- src/main/java/org/lastaflute/db/dbflute/classification/TypicalListedClassificationProvider.java
+++ src/main/java/org/lastaflute/db/dbflute/classification/TypicalListedClassificationProvider.java
@@ -22,6 +22,7 @@ import org.dbflute.jdbc.ClassificationMeta;
 import org.dbflute.optional.OptionalObject;
 import org.dbflute.optional.OptionalThing;
 import org.dbflute.util.Srl;
+import org.lastaflute.core.util.LaClassificationUtil;
 import org.lastaflute.db.dbflute.exception.ProvidedClassificationNotFoundException;
 
 /**
@@ -29,18 +30,22 @@ import org.lastaflute.db.dbflute.exception.ProvidedClassificationNotFoundExcepti
  */
 public abstract class TypicalListedClassificationProvider implements ListedClassificationProvider {
 
+    // ===================================================================================
+    //                                                                        Provide Meta
+    //                                                                        ============
     @Override
     public ClassificationMeta provide(String classificationName) throws ProvidedClassificationNotFoundException {
-        final ClassificationMeta onMainSchema = findOnMainSchema(classificationName);
-        if (onMainSchema == null) {
+        final ClassificationMeta meta = resolveMeta(classificationName);
+        if (meta == null) {
             handleClassificationNotFound(classificationName);
         }
-        return onMainSchema;
+        return meta;
     }
 
-    protected ClassificationMeta findOnMainSchema(String classificationName) throws ProvidedClassificationNotFoundException {
+    protected ClassificationMeta resolveMeta(String classificationName) // returns null when not found
+            throws ProvidedClassificationNotFoundException { // and throws when project not found
         final String projectDelimiter = getProjectDelimiter(); // dot means group delimiter so use other mark here
-        if (classificationName.contains(projectDelimiter)) { // e.g. sea$land: means land classification in sea project
+        if (classificationName.contains(projectDelimiter)) { // e.g. sea-land: means land classification in sea project
             final String projectName = Srl.substringFirstFront(classificationName, projectDelimiter);
             final String pureName = Srl.substringFirstRear(classificationName, projectDelimiter);
             return chooseClassificationFinder(projectName).apply(pureName);
@@ -50,18 +55,64 @@ public abstract class TypicalListedClassificationProvider implements ListedClass
     }
 
     protected String getProjectDelimiter() {
-        return "-";
+        return "-"; // e.g. maihamadb-MemberStatus
     }
 
+    /**
+     * Choose DB classification finder for the project. <br>
+     * (not contains application classification)
+     * <pre>
+     * e.g.
+     *  protected Function&lt;String, ClassificationMeta&gt; chooseClassificationFinder(String projectName)
+     *          throws ProvidedClassificationNotFoundException {
+     *      if (DBCurrent.getInstance().projectName().equals(projectName)) {
+     *          return clsName -&gt; onMainSchema(clsName).orElse(null); // null means not found
+     *      } else {
+     *          throw new ProvidedClassificationNotFoundException("Unknown DBFlute project name: " + projectName);
+     *      }
+     *  }
+     * </pre>
+     * @param projectName The project name of DBFlute. (NotNull)
+     * @return The finder of classification as function "from classification name to null-allowed meta". (NotNull)
+     * @throws ProvidedClassificationNotFoundException When the project is not found.
+     */
     protected abstract Function<String, ClassificationMeta> chooseClassificationFinder(String projectName)
             throws ProvidedClassificationNotFoundException;
 
+    /**
+     * Get default classification finder when project name is not specified. <br>
+     * Basically it returns main schema's classification. <br>
+     * (you can contain application classification)
+     * <pre>
+     * e.g.
+     *  protected Function&lt;String, ClassificationMeta&gt; getDefaultClassificationFinder() {
+     *      return clsName -&gt; {
+     *          return onMainSchema(clsName).orElseGet(() -&gt; {
+     *              return onAppCls(clsName).orElse(null); // null means not found
+     *          });
+     *      };
+     *  }
+     * </pre>
+     * @return The finder of classification as function "from classification name to null-allowed meta". (NotNull)
+     */
     protected abstract Function<String, ClassificationMeta> getDefaultClassificationFinder();
 
+    // -----------------------------------------------------
+    //                                          Assist Logic
+    //                                          ------------
+    // helper for sub class
+    protected OptionalThing<ClassificationMeta> findMeta(Class<?> defmetaType, String classificationName) {
+        return LaClassificationUtil.findMeta(defmetaType, classificationName);
+    }
+
+    // you can customize it when not found
     protected void handleClassificationNotFound(String classificationName) throws ProvidedClassificationNotFoundException {
         throw new ProvidedClassificationNotFoundException("Not found the classification: " + classificationName);
     }
 
+    // ===================================================================================
+    //                                                                     Delimiter Alias
+    //                                                                     ===============
     @Override
     public OptionalThing<String> determineAlias(Locale locale) {
         return OptionalObject.empty();
