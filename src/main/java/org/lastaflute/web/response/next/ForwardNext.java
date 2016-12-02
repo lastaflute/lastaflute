519a0ff62c7e165a1dc2f5414238c73aea8aacf7
jflute <dbflute@gmail.com>
2016-12-02 18:40:43 +0900

    hashCode(), toString() for unit test comparison

--- src/main/java/org/lastaflute/web/response/next/ForwardNext.java
+++ src/main/java/org/lastaflute/web/response/next/ForwardNext.java
@@ -20,8 +20,14 @@ package org.lastaflute.web.response.next;
  */
 public class ForwardNext implements RoutingNext {
 
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
     protected final String forwardPath;
 
+    // ===================================================================================
+    //                                                                         Constructor
+    //                                                                         ===========
     public ForwardNext(String forwardPath) {
         if (forwardPath == null) {
             throw new IllegalArgumentException("The argument 'forwardPath' should not be null.");
@@ -29,11 +35,31 @@ public class ForwardNext implements RoutingNext {
         this.forwardPath = forwardPath;
     }
 
+    // ===================================================================================
+    //                                                                      Basic Override
+    //                                                                      ==============
+    @Override
+    public int hashCode() {
+        return 31 * forwardPath.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof ForwardNext)) {
+            return false;
+        }
+        final ForwardNext you = ((ForwardNext) obj);
+        return forwardPath.equals(you.forwardPath);
+    }
+
     @Override
     public String toString() {
         return "forward:{" + forwardPath + "}";
     }
 
+    // ===================================================================================
+    //                                                                            Accessor
+    //                                                                            ========
     @Override
     public String getRoutingPath() {
         return forwardPath;
