deb79c97e8b01c15e6335c7912351aca7f462ebe
jflute <jflute@jflute-nyabara>
2019-12-24 12:22:12 +0900

    just fix: add exception handling notice to bridgeState() javadoc

--- src/main/java/org/lastaflute/core/magic/async/bridge/AsyncStateBridge.java
+++ src/main/java/org/lastaflute/core/magic/async/bridge/AsyncStateBridge.java
@@ -42,12 +42,15 @@ public class AsyncStateBridge {
      * AsyncStateBridge <span style="color: #553000">bridge</span> = <span style="color: #0000C0">asyncManager</span>.<span style="color: #994747">bridgeState</span>(<span style="color: #553000">op</span> <span style="font-size: 120%">-</span>&gt;</span> {});
      * yourOtherAsync.something(() <span style="font-size: 120%">-</span>&gt;</span> {
      *     <span style="color: #553000">bridge</span>.<span style="color: #CC4747">cross</span>(() <span style="font-size: 120%">-</span>&gt;</span> { <span style="color: #3F7E5E">// inherits caller thread states</span>
+     *         ... <span style="color: #3F7E5E">// non-transactional process here</span>
      *         <span style="color: #0000C0">transactionStage</span>.requiresNew(tx <span style="font-size: 120%">-</span>&gt;</span> { <span style="color: #3F7E5E">// should be in cross()</span>
      *             ... <span style="color: #3F7E5E">// asynchronous process here e.g. insert(), update()</span>
      *         });
      *     });
      * );
      * </pre>
+     * <p>The rule is same as async(), e.g. ThreadCacheContext, AccessContext, CallbackContext, ExceptionHandling.
+     * Also it contains exception handling (error logging) so you does not need to catch it basically.</p>
      * @param noArgLambda The callback for application logic as asynchronous process that has caller thread states. (NotNull)
      */
     public void cross(IndependentProcessor noArgLambda) {
