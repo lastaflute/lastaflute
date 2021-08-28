f8e584f71b0718b652f4bfa33a7fb17b17e5f81b
jflute <jflute@localhost>
2021-08-28 19:15:53 +0900

    improve doDetermineRestfulPath() by isIllegalResourceName()

--- src/main/java/org/lastaflute/web/path/restful/router/PairBasedRestfulRouter.java
+++ src/main/java/org/lastaflute/web/path/restful/router/PairBasedRestfulRouter.java
@@ -23,6 +23,11 @@ import org.dbflute.util.Srl;
 import org.lastaflute.web.path.UrlMappingResource;
 
 /**
+ * The RESTful router on pair-elements basis.
+ * <pre>
+ * o you can use complete string ID as default
+ * o you CANNOT use event-suffix e.g. get$sea()
+ * </pre>
  * @author jflute
  * @since 1.2.1 (2021/05/20)
  */
@@ -39,8 +44,8 @@ public class PairBasedRestfulRouter extends AbstractBasedRestfulRouter {
         // best effort logic and RESTful is prior in the application when using restful router
         int index = 0;
         for (String element : elementList) {
-            if (isIdElement(element)) { // e.g. 1
-                if (index % 2 == 0) { // first, third... e.g. /[1]/products/, /products/1/[2]/purchases
+            if (index % 2 == 0) { // first, third... e.g. /[products]/1/, /[products]/1/[purchases]/2/
+                if (isIllegalResourceName(element)) { // e.g. /[1]/products/, /products/1/[2]/purchases/
                     return false;
                 }
             }
@@ -50,9 +55,8 @@ public class PairBasedRestfulRouter extends AbstractBasedRestfulRouter {
         return true;
     }
 
-    // used by determination only
-    protected boolean isIdElement(String element) { // you can override for conventional string ID
-        return Srl.isNumberHarfAll(element); // numeric ID as default
+    protected boolean isIllegalResourceName(String element) {
+        return Srl.isNumberHarfAll(element); // cannot use numeric resource name
     }
 
     // -----------------------------------------------------
@@ -62,22 +66,37 @@ public class PairBasedRestfulRouter extends AbstractBasedRestfulRouter {
     protected String doConvertToMappingPath(String requestPath) {
         final List<String> elementList = splitPath(requestPath);
         final List<String> resourceList = new ArrayList<>();
-        final List<String> parameterList = new ArrayList<>();
+        final List<String> idList = new ArrayList<>();
         int index = 0;
         for (String element : elementList) {
             if (index % 2 == 0) { // first, third...
-                if (element.contains("-")) { // e.g. ballet-dancers
-                    resourceList.addAll(splitResource(element, "-")); // for e.g. ballet/dancers
-                } else {
-                    resourceList.add(element);
-                }
+                handleResourceElementOnMappingPath(resourceList, element);
             } else { // second, fourth
-                parameterList.add(element);
+                handleIdElementOnMappingPath(idList, element);
             }
             ++index;
         }
-        resourceList.addAll(parameterList); // e.g. /products/purchases/1/2/
-        return buildPath(resourceList);
+        final List<String> pathElementList = arrangePathElementList(resourceList, idList);
+        return buildPath(pathElementList);
+    }
+
+    protected void handleResourceElementOnMappingPath(List<String> resourceList, String element) {
+        if (element.contains("-")) { // e.g. ballet-dancers
+            resourceList.addAll(splitResource(element, "-")); // for e.g. ballet/dancers
+        } else {
+            resourceList.add(element);
+        }
+    }
+
+    protected void handleIdElementOnMappingPath(List<String> parameterList, String element) {
+        parameterList.add(element);
+    }
+
+    protected List<String> arrangePathElementList(List<String> resourceList, List<String> idList) {
+        final List<String> pathElementList = new ArrayList<>();
+        pathElementList.addAll(resourceList); // e.g. /products/purchases/
+        pathElementList.addAll(idList); // e.g. /products/purchases/1/2/
+        return pathElementList;
     }
 
     // ===================================================================================
